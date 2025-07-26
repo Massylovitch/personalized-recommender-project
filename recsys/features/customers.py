46bf50b44d3f742a120f9e2106f0134e49e510ca
Massyl GH <massyl-6@hotmail.fr>
2025-07-26 17:11:38 +0200

    Add customers data proc

---
+++ recsys/features/customers.py
@@ -0,0 +1,55 @@
+import random
+
+import polars as pl
+from loguru import logger
+
+from recsys.config import CustomerDatasetSize
+
+
+def compute_features_customers(
+    df, drop_null_age= False
+):
+
+    required_columns = ["customer_id", "club_member_status", "age", "postal_code"]
+    missing_columns = [col for col in required_columns if col not in df.columns]
+    if missing_columns:
+        raise ValueError(
+            f"Columns {', '.join(missing_columns)} not found in the DataFrame"
+        )
+
+    df = (
+        df.pipe(fill_missing_club_member_status)
+        .pipe(drop_na_age)
+        .with_columns([create_age_group(), pl.col("age").cast(pl.Float64)])
+        .select(
+            ["customer_id", "club_member_status", "age", "postal_code", "age_group"]
+        )
+    )
+
+    if drop_null_age is True:
+        df = df.drop_nulls(subset=["age"])
+
+    return df
+
+def fill_missing_club_member_status(df):
+    return df.with_columns(pl.col("club_member_status").fill_null("ABSENT"))
+
+def drop_na_age(df):
+    return df.drop_nulls(subset=["age"])
+
+def create_age_group():
+    return (
+        pl.when(pl.col("age").is_between(0, 18))
+        .then(pl.lit("0-18"))
+        .when(pl.col("age").is_between(19, 25))
+        .then(pl.lit("19-25"))
+        .when(pl.col("age").is_between(26, 35))
+        .then(pl.lit("26-35"))
+        .when(pl.col("age").is_between(36, 45))
+        .then(pl.lit("36-45"))
+        .when(pl.col("age").is_between(46, 55))
+        .then(pl.lit("46-55"))
+        .when(pl.col("age").is_between(56, 65))
+        .then(pl.lit("56-65"))
+        .otherwise(pl.lit("66+"))
+    ).alias("age_group")
