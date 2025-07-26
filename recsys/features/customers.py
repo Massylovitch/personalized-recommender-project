924c1c0c6a607608af86e8d8f991e2976f38e123
Massyl GH <massyl-6@hotmail.fr>
2025-07-26 17:22:04 +0200

    Add transactions data proc

--- recsys/features/customers.py
+++ recsys/features/customers.py
@@ -6,9 +6,7 @@ from loguru import logger
 from recsys.config import CustomerDatasetSize
 
 
-def compute_features_customers(
-    df, drop_null_age= False
-):
+def compute_features_customers(df, drop_null_age=False):
 
     required_columns = ["customer_id", "club_member_status", "age", "postal_code"]
     missing_columns = [col for col in required_columns if col not in df.columns]
@@ -31,11 +29,14 @@ def compute_features_customers(
 
     return df
 
+
 def fill_missing_club_member_status(df):
     return df.with_columns(pl.col("club_member_status").fill_null("ABSENT"))
 
+
 def drop_na_age(df):
     return df.drop_nulls(subset=["age"])
+
 
 def create_age_group():
     return (
@@ -53,3 +54,37 @@ def create_age_group():
         .then(pl.lit("56-65"))
         .otherwise(pl.lit("66+"))
     ).alias("age_group")
+
+
+class DatasetSampler:
+    _SIZES = {
+        CustomerDatasetSize.LARGE: 50_000,
+        CustomerDatasetSize.MEDIUM: 5_000,
+        CustomerDatasetSize.SMALL: 1_000,
+    }
+
+    def __init__(self, size: CustomerDatasetSize):
+        self._size = size
+
+    @classmethod
+    def get_supported_sizes(cls) -> dict:
+        return cls._SIZES
+
+    def sample(self, customers_df, transations_df):
+        random.seed(27)
+
+        n_customers = self._SIZES[self._size]
+        logger.info(f"Sampling {n_customers} customers.")
+        customers_df = customers_df.sample(n=n_customers)
+
+        logger.info(
+            f"Number of transactions for all the customers: {transations_df.height}"
+        )
+        transations_df = transations_df.join(
+            customers_df.select("customer_id"), on="customer_id"
+        )
+        logger.info(
+            f"Number of transactions for the {n_customers} sampled customers: {transations_df.height}"
+        )
+
+        return {"customers": customers_df, "transactions": transations_df}
